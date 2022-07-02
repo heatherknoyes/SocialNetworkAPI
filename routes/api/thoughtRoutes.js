d406992cbc4135906e113c5f89e6a3d61a5d800f
Heather Noyes <heatherknoyes@gmail.com>
2022-07-02 09:27:32 -0400

    Updated thought routes code

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const {
+  getThoughts,
+  createThought,
+  getSingleThought,
+  deleteSingleThought,
+  updateSingleThought,
+  createReaction,
+  deleteReaction,
+} = require("../../controllers/thoughtController");
+
+// /api/thoughts
+router.route("/").get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .delete(deleteSingleThought)
+  .put(updateSingleThought);
+
+// /api/thoughts/:thoughtId/reactions
+router
+  .route("/:thoughtId/reactions")
+  .post(createReaction)
+  .delete(deleteReaction);
+
+module.exports = router;
