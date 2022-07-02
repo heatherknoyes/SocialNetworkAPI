aba39fa369eed92852ede7e7a6a2f2941bda7d45
Heather Noyes <heatherknoyes@gmail.com>
2022-07-02 09:23:43 -0400

    Added userRoutes.js with proposed methods

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const {
+  getUsers,
+  createUser,
+  getSingleUser,
+  deleteSingleUser,
+  updateSingleUser,
+  createFriend,
+  deleteFriend,
+} = require("../../controllers/userController");
+
+// /api/users
+router.route("/").get(getUsers).post(createUser);
+
+// /api/users/:userId
+router
+  .route("/:userId")
+  .get(getSingleUser)
+  .delete(deleteSingleUser)
+  .put(updateSingleUser);
+
+// /api/users/:userId/friends/:friendId
+router
+  .route("/:userId/friends/:friendId")
+  .post(createFriend)
+  .delete(deleteFriend);
+
+module.exports = router;
