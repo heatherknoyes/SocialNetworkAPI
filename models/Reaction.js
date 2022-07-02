c38f8ee673f430f0af84bdd99d133c54383ddd6e
Heather Noyes <heatherknoyes@gmail.com>
2022-07-02 09:15:12 -0400

    Added starter code for schemas

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+// const assignmentSchema = require('./Assignment');
+
+// Schema to create Student model
+const reactionSchema = new Schema(
+  {
+    first: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    last: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    github: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    assignments: [assignmentSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Reaction = model("reaction", reactionSchema);
+
+module.exports = Reaction;
